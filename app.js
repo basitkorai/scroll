13ac9f14d333a2375118ef525b8cf2f61d7f864d
Basit Korai <basitalikorai7@gmail.com>
2023-04-16 09:16:18 +0500

    back to top btn added

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ navToggle.addEventListener("click", () => {
   }
 });
 
+const topLink = document.querySelector(".top-link");
 // ********** fixed navbar ************
 
 window.addEventListener("scroll", () => {
@@ -32,6 +33,12 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("fixed-nav");
   } else {
     navbar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
   }
 });
 
